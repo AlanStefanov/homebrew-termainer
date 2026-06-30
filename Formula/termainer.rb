55388ac0ba4cb41e7afccb0dfb6f7f8a56a176b6
publish.sh <dev@local>
2026-06-30 16:40:27 -0300

    chore: bump termainer to v0.5.2a0

--- Formula/termainer.rb
+++ Formula/termainer.rb
@@ -1,12 +1,12 @@
-# version: 0.5.1
+# version: 0.5.2a0
 
 class Termainer < Formula
   include Language::Python::Virtualenv
 
   desc "Container observability and operations directly from your terminal"
   homepage "https://github.com/AlanStefanov/termainer"
-  url "https://files.pythonhosted.org/packages/source/t/termainer/termainer-0.5.1.tar.gz"
-  sha256 "119043123eb10169ca35e02a77241956058626300a716c00c4e3e3f91e5011f4"
+  url "https://files.pythonhosted.org/packages/source/t/termainer/termainer-0.5.2a0.tar.gz"
+  sha256 "4362fdf0c3899d421e089f5fc187f85f5051c2667300ff82a7634c3b3923bd5f"
   license "MIT"
 
   depends_on "python@3.11"
